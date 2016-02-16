c83324efe7070c6a8a00045d91c46300d066565e
mikhael <mikhael@milikhin.name>
2016-02-16 14:15:48 +0600

    add git init

--- setup.js
+++ setup.js
@@ -14,6 +14,7 @@ R5mInit.prototype.check = function(programName) {
 R5mInit.prototype.initRepo = function() {
   shell.exec('git clone https://github.com/milikhin/r5m-landing.git ' + this.path);
   shell.exec('cd ' + this.path + '; npm i');
+  shell.exec('rm -rf .git; git init');
 }
 
 R5mInit.prototype.initClient = function() {
